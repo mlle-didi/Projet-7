ece76050cd5999bab4c3fc1d97a6478824b9d767
Cindy Leroy <mlle.cindy.leroy@gmail.com>
2022-12-26 09:21:45 +0100

    Création du router

---
+++ src/routes/RoutesPath.jsx
@@ -0,0 +1,29 @@
+// React Framework
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
+import Layout from '../components/Layout/Layout';
+
+// Pages
+import Home from '../pages/Home/Home'
+import About from '../pages/About/About'
+import FicheLogement from '../pages/FicheLogement/FicheLogement'
+import Error from '../pages/Error/Error'
+
+const RoutesPath = () => {
+  return (
+    <BrowserRouter>
+    <Layout>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/logements/:id" element={<FicheLogement/>}/>
+            <Route path="*" element={<Error/>}/>
+        </Routes>
+    </Layout>
+    </BrowserRouter>
+  )
+}
+
+export default RoutesPath
