bba9ab6eb5ff299e267f92d03828a5ec28eea282
Cindy Leroy <mlle.cindy.leroy@gmail.com>
2022-12-26 09:19:26 +0100

    Initialisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// React Framework
+import React from 'react'
+
+// Routes
+import RoutesPath from './routes/RoutesPath'
+
+function App() {
+    return (
+        <div className='app' >
+            <RoutesPath/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+// React Framework
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// App.jsx
+import App from "./App";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
