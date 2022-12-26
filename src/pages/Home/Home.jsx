1688ecd9d821c0b30eb424183b5fc038f915eaac
Cindy Leroy <mlle.cindy.leroy@gmail.com>
2022-12-26 09:47:05 +0100

    Création page d'accueil + CSS à jour

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,37 @@
+// React Framework
+import { Link } from 'react-router-dom'
+
+// Components
+import Banner from '../../components/Banner/Banner'
+import Card from '../../components/Card/Card'
+
+// Data
+import logements from '../../data.json'
+
+// Assets
+import bannerHome from '../../assets/images/BANNER_HOME.png' 
+
+// Style
+import "../Home/Home.css"
+
 function Home() {
   return (
     <div>
-      
+      <Banner banner={bannerHome} text="Chez vous, partout et ailleurs"/>
+      <div className="card-background margin-LR">
+			  <div className='container-card'>
+				  {
+					  logements.map( (logements) => {
+						  return(
+                <Link key={logements.id} to={"/logements/"+logements.id}> 
+                  <Card key={logements.id} cover={logements.cover} title={logements.title} id={logements.id} /> 
+                </Link>      
+              )
+            })
+          }
+        </div>
+      </div>
+      <h1> </h1>
     </div>
   )
 }
