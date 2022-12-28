71db564d1d2258d954173ea3aa8b4f4ff963bddd
Cindy Leroy <mlle.cindy.leroy@gmail.com>
2022-12-28 09:35:34 +0100

    MAJ menu active

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import React from 'react'
 // Routes
 import RoutesPath from './routes/RoutesPath'
 
+// Style
+import "../src/App.css"
+
+
 function App() {
     return (
-        <div className='app' >
+        <div>
             <RoutesPath/>
         </div>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,13 +8,22 @@ import logo from "../../assets/images/LOGO.svg";
 import "../Header/Header.css"
 
 function Header() {
+    let activeStyle = {textDecoration: "underline"};
     return (
         <header className='margin-LR margin-BT'>
             <nav>
                 <img className='logo' src={logo} alt="logo de l'agence kasa" />
                 <div className='menu'>
-                    <NavLink className='menu-link space-link' to="/">Accueil</NavLink>
-                    <NavLink className='menu-link' to="/about">A Propos</NavLink>
+                    <NavLink 
+                        className='menu-link space-link'
+                        style={({ isActive }) =>isActive ? activeStyle : undefined}
+                        to="/">Accueil
+                    </NavLink>
+                    <NavLink 
+                        className='menu-link'
+                        style={({ isActive }) =>isActive ? activeStyle : undefined}
+                        to="/about">A Propos
+                    </NavLink>
                 </div>
             </nav>
         </header>
