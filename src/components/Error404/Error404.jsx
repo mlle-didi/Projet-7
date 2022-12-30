87a9750bff7190091cc121c2719895b5dd36d4ea
Cindy Leroy <mlle.cindy.leroy@gmail.com>
2022-12-30 13:04:04 +0100

    MAJ page Error404

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -8,7 +8,7 @@ function Error() {
   return (
     <div className="error-container">
       <h1 className="error-title">404</h1>
-      <h2 className="error-text">Oups! La page que vous demandez n'existe pas</h2>
+      <h2 className="error-text">Oups! La page que vous demandez n'existe pas.</h2>
       <Link className='error-link' to="/">Retourner sur la page d’accueil</Link>
     </div>
   )
